a469c69c714f362aacd3161e78152a332d1dcd2d
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-05-25 10:34:15 +0400

    transaction row preview

--- ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/TransactionRow.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct TransactionRow: View {
+    var transaction: Transaction
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 20) {
+            
+        }
+        .padding([.top, .bottom], 8)
     }
 }
 
 struct TransactionRow_Previews: PreviewProvider {
     static var previews: some View {
-        TransactionRow()
+        Group {
+            TransactionRow(transaction: transactionPreviewData)
+            TransactionRow(transaction: transactionPreviewData)
+                .preferredColorScheme(.dark)
+        }
     }
 }
