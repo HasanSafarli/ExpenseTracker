5d672fe39376f6b69eb9c24b9ba31a4ead5380e9
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-05-25 09:22:19 +0400

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Hasan Safarli on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
