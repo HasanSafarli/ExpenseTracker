f23fdd5c0cccd47023e0d57cfffbebe81ba118f9
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-05-25 10:39:07 +0400

    transaction row preview show

--- ExpenseTracker/TransactionRow.swift
+++ ExpenseTracker/TransactionRow.swift
@@ -11,6 +11,24 @@ struct TransactionRow: View {
     var transaction: Transaction
     var body: some View {
         HStack(spacing: 20) {
+            VStack(alignment: .leading, spacing: 6) {
+                //MARK: Transaction Merchant
+                Text(transaction.merchant)
+                    .font(.subheadline)
+                    .bold()
+                    .lineLimit(1)
+                
+                //MARK: Category Info
+                Text(transaction.category)
+                    .font(.footnote)
+                    .opacity(0.7)
+                    .lineLimit(1)
+                
+                //MARK: Transaction Date
+                Text(Date(), format: .dateTime.year().month().day())
+                    .font(.footnote)
+                    .foregroundColor(.secondary)
+            }
             
         }
         .padding([.top, .bottom], 8)
