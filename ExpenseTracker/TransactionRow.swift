008680483c55acf91d4d5160187f706bf30e2d9f
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-05-25 10:25:51 +0400

    transactions

---
+++ ExpenseTracker/TransactionRow.swift
@@ -0,0 +1,20 @@
+//
+//  TransactionRow.swift
+//  ExpenseTracker
+//
+//  Created by Hasan Safarli on 5/25/23.
+//
+
+import SwiftUI
+
+struct TransactionRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TransactionRow_Previews: PreviewProvider {
+    static var previews: some View {
+        TransactionRow()
+    }
+}
