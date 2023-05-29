b21195c423f30c6c90e9fd85238304cf758df1f2
HasanSafarli <hasansafarli@knights.ucf.edu>
2023-05-29 14:32:27 +0400

    dark mode on transaction list

---
+++ ExpenseTracker/TransactionList.swift
@@ -0,0 +1,40 @@
+//
+//  TransactionList.swift
+//  ExpenseTracker
+//
+//  Created by Hasan Safarli on 5/29/23.
+//
+
+import SwiftUI
+
+struct TransactionList: View {
+    @EnvironmentObject var transactionListVM: TransactionListViewModel
+    var body: some View {
+        VStack {
+            
+        }
+        .navigationTitle("Transactions")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct TransactionList_Previews: PreviewProvider {
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel()
+        transactionListVM.transactions = transactionListPreviewData
+        return transactionListVM
+    }()
+
+    static var previews: some View {
+        Group {
+            NavigationView {
+                TransactionList()
+            }
+            NavigationView {
+                TransactionList()
+                    .preferredColorScheme(.dark)
+            }
+        }
+        .environmentObject(transactionListVM)
+    }
+}
